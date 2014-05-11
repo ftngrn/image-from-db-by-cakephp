ec9030019a3f4a310fb9143c86e7d53c549026be
Noriaki Ishihara <ishihara.noriaki+exlock@gmail.com>
2014-05-12 00:53:23 +0900

    Add source codes

---
+++ Test/Fixture/ImageFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * ImageFixture
+ *
+ */
+class ImageFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'body' => array('type' => 'binary', 'null' => true, 'default' => null),
+		'mime' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 64, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'body' => 'Lorem ipsum dolor sit amet',
+			'mime' => 'Lorem ipsum dolor sit amet',
+			'created' => '2014-05-07 23:32:39'
+		),
+	);
+
+}
